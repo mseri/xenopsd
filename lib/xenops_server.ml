99113693680a72d3bc952bf99f360cc29c882cba
Marcello Seri <marcello.seri@citrix.com>
2017-04-21 10:59:15 +0100

    xenops_server: DANGER disable memory_limit call
    I expect migration to fail a priori but it's worth a try.
    
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- lib/xenops_server.ml
+++ lib/xenops_server.ml
@@ -1539,14 +1539,14 @@ and perform ?subtask ?result (op: operation) (t: Xenops_task.t) : unit =
 						Request.write (fun _ -> ()) request fd
 					) in
 
-					do_request mem_fd ["memory_limit", Int64.to_string state.Vm.memory_limit] memory_url;
+					(*do_request mem_fd ["memory_limit", Int64.to_string state.Vm.memory_limit] memory_url;
 
 					begin match Handshake.recv mem_fd with
 						| Handshake.Success -> ()
 						| Handshake.Error msg ->
 							error "cannot transmit vm to host: %s" msg;
 							raise (Internal_error msg)
-					end;
+					end;*)
 
 					debug "VM.migrate: Synchronisation point 1";
 
