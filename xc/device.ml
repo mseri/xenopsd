524d112bd0382bf6e2e69fab5de217a8216bb5bc
Marcello Seri <marcello.seri@citrix.com>
2017-04-21 10:35:39 +0100

    device: add more hacks

--- xc/device.ml
+++ xc/device.ml
@@ -1737,7 +1737,7 @@ let start_vgpu ~xs task ?(restore = false) ?restore_fd domid vgpus vcpus =
 		(* Start DEMU and wait until it has reached the "initialising" or "restoring" state *)
 		let state_path = Printf.sprintf "/local/domain/%d/vgpu/state" domid in
 		let cancel = Cancel_utils.Vgpu domid in
-		if not (Vgpu.is_running ~xs domid) then begin
+		let should_wait = if not (Vgpu.is_running ~xs domid) then begin
 			(* The below line does nothing if the device is already bound to the
 			 * nvidia driver. We rely on xapi to refrain from attempting to run
 			 * a vGPU on a device which is passed through to a guest. *)
@@ -1761,28 +1761,33 @@ let start_vgpu ~xs task ?(restore = false) ?restore_fd domid vgpus vcpus =
 					PCI.bind [vgpu.physical_pci_address] PCI.Nvidia;
 					Some fds
 			in match maybe_fds with
-			| None -> ()
+			| None -> false
 			| Some fds ->
 				let args = vgpu_args_of_nvidia domid vcpus vgpu fds in
 				let vgpu_pid = init_daemon ~task ~path:!Xc_resources.vgpu ~args
 					~name:"vgpu" ~domid ~xs ~ready_path:state_path ~timeout:!Xenopsd.vgpu_ready_timeout
 					~cancel ~fds () in
 				Forkhelpers.dontwaitpid vgpu_pid;
-		end else
+				true
+		end else begin
 			info "Daemon %s is already running for domain %d" !Xc_resources.vgpu domid;
-
-		(* Keep waiting until DEMU's state becomes "running", which means that it is
-		 * ready to run the VM. *)
-		let good_watch = Watch.value_to_become state_path "running" in
-		let error_watch = Watch.value_to_become state_path "error" in
-		if cancellable_watch cancel [ good_watch ] [ error_watch ] task ~xs ~timeout:3600. () then
-			info "Daemon vgpu is ready"
-		else begin
-			let error_code_path = Printf.sprintf "/local/domain/%d/vgpu/error-code" domid in
-			let error_code = xs.Xs.read error_code_path in
-			error "Daemon vgpu returned error: %s" error_code;
-			raise (Ioemu_failed ("vgpu", Printf.sprintf "Daemon vgpu returned error: %s" error_code))
-		end
+			true
+		end in
+		if should_wait then begin
+			(* Keep waiting until DEMU's state becomes "running", which means that it is
+			* ready to run the VM. *)
+			let good_watch = Watch.value_to_become state_path "running" in
+			let error_watch = Watch.value_to_become state_path "error" in
+			if cancellable_watch cancel [ good_watch ] [ error_watch ] task ~xs ~timeout:3600. () then
+				info "Daemon vgpu is ready"
+			else begin
+				let error_code_path = Printf.sprintf "/local/domain/%d/vgpu/error-code" domid in
+				let error_code = xs.Xs.read error_code_path in
+				error "Daemon vgpu returned error: %s" error_code;
+				raise (Ioemu_failed ("vgpu", Printf.sprintf "Daemon vgpu returned error: %s" error_code))
+			end
+		end else
+			debug "should_wait = false; proceeding without vgpu";
 	| [{implementation = GVT_g vgpu}] ->
 		PCI.bind [vgpu.physical_pci_address] PCI.I915
 	| [{implementation = MxGPU vgpu}] ->
